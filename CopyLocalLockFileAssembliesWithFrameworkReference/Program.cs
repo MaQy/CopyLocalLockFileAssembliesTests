a69e6186373ca24e1bf0779a85678b9824ce00aa
Borrego Polo, Fernando <fbpolo@ea.com>
2021-04-21 23:05:49 +0200

    Add project files.

---
+++ CopyLocalLockFileAssembliesWithFrameworkReference/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CopyLocalLockFileAssembliesWithFrameworkReference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
